d090f994fca3d0461a42f844c30952c8c489d74d
Ethan Bates <119895540+milkwxter@users.noreply.github.com>
2025-03-31 08:57:40 -0500

    Add files via upload

---
+++ source code/StatWorker_HediffAdder.cs
@@ -0,0 +1,70 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityStandardAssets.ImageEffects;
+using VanillaWeaponsExpandedLaser;
+using Verse;
+
+namespace DestinyMod
+{
+    public class StatWorker_HediffAdder : StatWorker
+    {
+        public bool ValidReq(StatRequest req)
+        {
+            if(req.Thing != null && req.Thing.def != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override string GetExplanationFinalizePart(StatRequest req, ToStringNumberSense numberSense, float finalVal)
+        {
+            if (ValidReq(req))
+            {
+                // get the projectile
+                ThingDef projectile = req.Thing.def.Verbs[0].defaultProjectile;
+
+                if (projectile is ProjectileDestinyDef projectileDestinyDef)
+                {
+                    return $"Inflicts {projectileDestinyDef.hediffToAdd.label} with a chance of {projectileDestinyDef.hediffChance * 100}% on hit.";
+                }
+                else if (projectile is LaserBeamDestinyDef laserBeamDestinyDef)
+                {
+                    return $"Inflicts {laserBeamDestinyDef.hediffToAdd.label} with a chance of {laserBeamDestinyDef.hediffChance * 100}% on hit.";
+                }
+            }
+            return base.GetExplanationFinalizePart(req, numberSense, finalVal);
+        }
+
+        public override string GetStatDrawEntryLabel(StatDef stat, float value, ToStringNumberSense numberSense, StatRequest optionalReq, bool finalized = true)
+        {
+            // get the projectile
+            ThingDef projectile = optionalReq.Thing.def.Verbs[0].defaultProjectile;
+
+            if (projectile is ProjectileDestinyDef projectileDestinyDef)
+            {
+                return projectileDestinyDef.hediffToAdd.label;
+            }
+            else if (projectile is LaserBeamDestinyDef laserBeamDestinyDef)
+            {
+                return laserBeamDestinyDef.hediffToAdd.label;
+            }
+            else
+            {
+                return "None";
+            }
+        }
+
+        public override bool ShouldShowFor(StatRequest req)
+        {
+            if (ValidReq(req))
+            {
+                if (req.Thing.def.Verbs[0].defaultProjectile is ProjectileDestinyDef || req.Thing.def.Verbs[0].defaultProjectile is LaserBeamDestinyDef)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ source code/StatusEffects.cs
@@ -0,0 +1,85 @@
+﻿using RimWorld;
+using System.Linq;
+using Verse.AI;
+using Verse;
+using UnityEngine;
+using System.Collections.Generic;
+using Verse.Sound;
+using VanillaWeaponsExpandedLaser;
+using static UnityEngine.UI.Image;
+
+namespace DestinyMod
+{
+    public class Hediff_DM_Arc : HediffWithComps
+    {
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (base.pawn.IsHashIntervalTick(180)) // Check every 180 ticks
+            {
+                // make sure pawn is still living
+                if (base.pawn.Dead)
+                {
+                    base.pawn.health.RemoveHediff(this);
+                    return;
+                }
+
+                // do a fleck
+                FleckMaker.AttachedOverlay(base.pawn, DefDatabase<FleckDef>.GetNamed("DS_Arc_Fleck"), new Vector3(0f, 0f, 0f));
+
+                // create a damage info
+                DamageInfo zapped = new DamageInfo(DamageDefOf.Burn, 10f, 1f, -1f, null);
+                List<Pawn> pawnsToZap = new List<Pawn>();
+
+                // get nearby pawns of same faction
+                foreach (IntVec3 cell in GenRadial.RadialCellsAround(pawn.Position, 5f, true))
+                {
+                    if (!cell.InBounds(pawn.Map)) continue;
+                    List<Thing> things = cell.GetThingList(pawn.Map);
+                    foreach (Thing thing in things)
+                    {
+                        if (thing is Pawn zapee)
+                        {
+                            // make sure only target only OTHER LIVING FRIENDLIES (to the guy who got zapped)
+                            if (zapee.Faction != null && zapee.Faction.HostileTo(pawn.Faction) && zapee != pawn) continue;
+
+                            // add them to the zap list
+                            pawnsToZap.Add(zapee);
+                        }
+                    }
+                }
+
+                // zapping time
+                int zapperTicksRemaining = 2 * 60;
+                foreach (Pawn pawnToZap in pawnsToZap)
+                {
+                    pawnToZap.TakeDamage(zapped);
+                    DefDatabase<SoundDef>.GetNamed("DS_Zap").PlayOneShot(new TargetInfo(pawnToZap.Position, pawnToZap.Map, false));
+
+                    // le epic effectz
+                    Vector3 start = pawn.DrawPos;
+                    Vector3 end = pawnToZap.DrawPos;
+                    Material zapMat = MaterialPool.MatFrom("Things/Projectile/Shot_ArcLightning");
+                    if (zapperTicksRemaining > 0)
+                    {
+                        zapperTicksRemaining--;
+                        GenDraw.DrawLineBetween(start, end, zapMat, 5f);
+                    }
+                    zapperTicksRemaining = 2 * 60;
+                }
+            }
+        }
+
+        public override void PostAdd(DamageInfo? dinfo)
+        {
+            base.PostAdd(dinfo);
+
+            // Throw a mote
+            MoteMaker.ThrowText(base.pawn.DrawPos, base.pawn.Map, "Arc proc", Color.cyan, 3f);
+
+            // do a fleck
+            FleckMaker.AttachedOverlay(base.pawn, DefDatabase<FleckDef>.GetNamed("DS_Arc_Fleck"), new Vector3(0f, 0f, 0f));
+        }
+    }
+}
